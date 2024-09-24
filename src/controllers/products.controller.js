5d373f7920857f9c0147d5934458d5e826308b22
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-09-24 07:59:58 -0400

    refactor: change text to test api get

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,7 +2,7 @@ import { getConnection, sql } from "../database/connection.js";
 
 export const getProducts = async (req, res) => {
   try {
-    console.log("GET request to the homepage");
+    console.log("test api");
   } catch (error) {
     throw new Error("Error al obtener los productos:", error);
   }
