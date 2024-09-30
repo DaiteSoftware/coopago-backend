0605eb06ad0ac8174ccfbe4c4be6ac09d67b711d
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-09-30 15:17:19 -0400

    style: eslint formatter

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import login from "./routes/login.routes.js";
-import { verifyUser } from "./middlewares/verifyDb.js";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import login from './routes/login.routes.js';
+import home from './routes/home.routes.js';
+import session from 'express-session';
+import { SESSION_SECRET } from './config.js';
 const app = express();
 
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 // Routes
-app.use("/api", verifyUser, login);
+app.use('/api', login);
+app.use('/api', home);
 
 export default app;
