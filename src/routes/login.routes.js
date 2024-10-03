0c37b1dc58570d382aae56cf8f50a97266941208
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-03 13:13:06 -0400

    feat: add logout functionality

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { login } from '../controllers/login.controller.js'
+import { login, logout } from '../controllers/login.controller.js'
 import { verifyUser } from '../middlewares/verifyDb.js'
 
 const router = Router()
 
 router.post('/login', verifyUser, login)
+router.post('/logout', logout)
 
 export default router
