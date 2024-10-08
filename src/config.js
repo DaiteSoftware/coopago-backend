9ec22b68c9bf3f2f8330b247c685f636fb9134f3
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-08 15:43:48 -0400

    refactor: remove unnecessary semicolons

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
-import { config } from 'dotenv';
-config();
+import { config } from 'dotenv'
+config()
 
-export const DB_PORT = process.env.DB_PORT;
-export const PORT = process.env.PORT;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_SERVER = process.env.DB_SERVER;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const FRONTEND_URL = process.env.FRONTEND_URL;
-export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const DB_PORT = process.env.DB_PORT
+export const PORT = process.env.PORT
+export const DB_USER = process.env.DB_USER
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_SERVER = process.env.DB_SERVER
+export const DB_DATABASE = process.env.DB_DATABASE
+export const FRONTEND_URL = process.env.FRONTEND_URL
+export const SESSION_SECRET = process.env.SESSION_SECRET
