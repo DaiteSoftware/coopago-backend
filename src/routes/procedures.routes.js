677f1493c409eef6146dcf847d0293deffcd16eb
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-13 13:27:32 -0400

    Refactor procedures.routes.js to include authentication middleware and execute stored procedures

--- src/routes/procedures.routes.js
+++ src/routes/procedures.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { getStoredProcedures, getProcedureParams } from '../controllers/procedures.controller.js'
+import { getStoredProcedures, getProcedureParams, executeStoredProcedure } from '../controllers/procedures.controller.js'
 import { isAuthenticated } from '../middlewares/auth.js'
+// import { verifyUser } from '../middlewares/verifyDb.js'
 
 const router = Router()
 
 router.get('/getStoredProcedures', isAuthenticated, getStoredProcedures)
-router.get('/getProcedureParams/:procedureName', getProcedureParams)
+router.get('/getProcedureParams/:procedureName', isAuthenticated, getProcedureParams)
+router.post('/ejecutar', executeStoredProcedure)
 
 export default router
