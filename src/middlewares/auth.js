640a921ba008e1821ffb463e3bb66bb670c019ed
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-14 13:23:48 -0400

    refactor: change name to session in middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,8 @@
 export const isAuthenticated = (req, res, next) => {
-  if (req.session.usuario) {
-    // Si el usuario está autenticado, pasamos al siguiente middleware o ruta
+  if (req.session.auth) {
     return next()
   } else {
     // Si no está autenticado, respondemos con un error 401
-    return res.status(401).json({ error: 'Usuario no autenticado' })
+    return res.status(401).json({ error: 'Usuario no autenticado middleware' })
   }
 }
