f9c6ec809fc0345789d9de2f331955cfb820120c
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-16 09:21:51 -0400

    refactor: connection configuration and remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import app from './app.js';
-import { PORT } from './config.js';
+import app from './app.js'
 import {
   DB_SERVER,
   DB_PORT,
   DB_DATABASE,
+  PORT,
   DB_USER,
-  DB_PASSWORD,
-} from './config.js';
-import { getConnection } from './database/connection.js';
+  DB_PASSWORD
+} from './config.js'
+import { getConnection } from './database/connection.js'
 
 const requiredEnvVars = {
   DB_SERVER,
   DB_PORT,
   DB_DATABASE,
   DB_USER,
-  DB_PASSWORD,
-};
+  DB_PASSWORD
+}
 
 for (const [key, value] of Object.entries(requiredEnvVars)) {
   if (!value) {
-    throw new Error(`${key} is missing`);
+    throw new Error(`${key} is missing`)
   }
 }
 
 app.listen(PORT, () => {
-  getConnection();
-});
-console.log('Server on port', PORT);
+  getConnection()
+})
+console.log('Server on port', PORT)
