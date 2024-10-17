11990ba48f4ed60c85d8ee4104229f3f79f4578e
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-17 11:42:39 -0400

    refactor: delete comments

--- src/database/reconection.js
+++ src/database/reconection.js
@@ -2,10 +2,10 @@ import sql from 'mssql'
 
 export const connectToDatabase = async (dbConfig) => {
   try {
-    await sql.close() // Cierra cualquier conexión previa
-    return await sql.connect(dbConfig) // Retorna una nueva conexión
+    await sql.close()
+    return await sql.connect(dbConfig)
   } catch (error) {
     console.error('Error connecting to database:', error)
-    throw error // Propaga el error para manejarlo más arriba
+    throw error
   }
 }
